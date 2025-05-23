58d1c60eb71a25b07b11f1b268c1e6a2cb273b5a
John <jworks2507@gmail.com>
2025-05-23 18:23:34 -0400

    Fixed error in keymappings; Starting to add some Primeagen keymaps

--- hosts/default/nvim/keymaps.lua
+++ hosts/default/nvim/keymaps.lua
@@ -1,12 +1,22 @@
 -- Define all keymaps here to make sure
 -- options.lua is loaded first
 
+--vim.g.mapleader = " "
 
 -- Telescope
+local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+-- General
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
 
