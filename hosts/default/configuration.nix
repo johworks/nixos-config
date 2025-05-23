30d88c59993b09eac875724317c9733980c4f193
John <jworks2507@gmail.com>
2025-05-23 12:33:53 -0400

    Fixed some renamed paths errors

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -64,7 +64,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -104,8 +104,8 @@
 
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "john";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "john";
 
   # Install firefox.
   programs.firefox.enable = true;
