3cd1a2862e64633b88ec4867b7bc347fcd9792e6
John <jworks2507@gmail.com>
2025-09-06 22:00:22 -0400

    Updated home-assistant to have a rolling counter of monthly energy used by the ac

--- modules/nixos/home-assistant.nix
+++ modules/nixos/home-assistant.nix
@@ -1,4 +1,7 @@
 {config, pkgs, ...}: 
+let
+  myDomain = "home.goobhub.org";
+in 
 {
 
   services.home-assistant = {
@@ -17,6 +20,34 @@
 
     config = {
       default_config = {};
+
+      # Turn W -> kWh
+      sensor = [
+        {
+          platform = "integration";
+          source = "sensor.ac_john_kauf_plug_power";
+          name = "John AC Energy kWh";
+          unit_prefix = "k";
+          round = 3;
+          method = "trapezoidal";
+          unit_time = "h";
+        }
+      ];
+
+      # Culumative counters
+      utility_meter = {
+        john_ac_energy_monthly = {
+          source = "sensor.john_ac_energy_kwh";
+          cycle = "monthly";
+        };
+
+        john_ac_energy_yearly = {
+          source = "sensor.john_ac_energy_kwh";
+          cycle = "yearly";
+        };
+      };
+
+
     };
 
     openFirewall = true;
@@ -27,6 +58,46 @@
   networking.firewall.allowedTCPPorts = [
     config.services.home-assistant.config.http.server_port
   ];
+
+
+  ######################################################################
+  ## Nginx reverse‑proxy with ACME
+  ######################################################################
+  #services.nginx = {
+  #  enable = true;
+
+  #  virtualHosts."${myDomain}" = {
+  #    forceSSL   = true;
+  #    enableACME = true;
+
+  #    locations."/" = {
+  #      proxyPass       = "http://127.0.0.1:8223";
+  #      proxyWebsockets = true;
+  #    };
+  #  };
+
+  #};
+
+
+  ######################################################################
+  ## ACME global settings
+  ######################################################################
+  #security.acme = {
+  #  acceptTerms = true;
+  #  # globally used for any host with enableACME = true
+  #  defaults = {
+  #    email  = "${myAcmeEmail}";
+  #  };
+  #};
+
+  ######################################################################
+  ## Firewall
+  ######################################################################
+  #networking.firewall = {
+  #  allowedTCPPorts = [ 80 443 ];
+  #  enable = true;
+  #};
+
 
   # Make sure that we're connected before trying to start
   systemd.services.home-assistant.after = [ "network-online.target" ];
