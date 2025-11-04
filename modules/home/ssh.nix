5c5b94914a41438e8f70e6b2a0e1213d00764973
John <jworks2507@gmail.com>
2025-11-03 21:45:10 -0500

    Added ssh alias for nuc

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -4,11 +4,15 @@
   programs.ssh = {
   	enable = true;
 	matchBlocks = {
-		"github.com" = {
-			user = "git";
-			hostname = "github.com";
-			identityFile = "~/.ssh/github_id_ed25519";
-		};
+      "github.com" = {
+        user = "git";
+        hostname = "github.com";
+        identityFile = "~/.ssh/github_id_ed25519";
+      };
+      "nuc" = {
+        hostname = "192.168.10.2"; # fixed as it's a router rn
+        identityFile = "~/.ssh/id_ed25519";
+      };
     };
 
     # Control known_hosts through home.file
