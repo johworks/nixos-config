50c12f15789b0bba07c1ecd24e6ab7da9b27abe6
John <jworks2507@gmail.com>
2026-06-09 19:04:48 -0400

    Add SearXNG basic auth

--- modules/nixos/searxng.nix
+++ modules/nixos/searxng.nix
@@ -5,10 +5,14 @@ let
   zoneName = "goobhub.org";
   stateDir = "/var/lib/searxng";
   environmentFile = "${stateDir}/searx.env";
+  authDir = "/var/lib/nginx/searxng";
+  htpasswdFile = "${authDir}/htpasswd";
+  passwordFile = "${authDir}/password";
 in
 {
   systemd.tmpfiles.rules = [
     "d ${stateDir} 0750 searx searx - -"
+    "d ${authDir} 0750 root nginx - -"
   ];
 
   systemd.services.searxng-secret = {
@@ -39,6 +43,37 @@ in
     after = [ "searxng-secret.service" ];
   };
 
+  systemd.services.searxng-basic-auth = {
+    description = "Create SearXNG basic auth credentials";
+    before = [ "nginx.service" ];
+    requiredBy = [ "nginx.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+      Group = "root";
+      UMask = "0077";
+    };
+    path = with pkgs; [
+      apacheHttpd
+      coreutils
+      openssl
+    ];
+    script = ''
+      install -d -m 0750 -o root -g nginx ${authDir}
+      if [ ! -s ${passwordFile} ]; then
+        openssl rand -base64 36 > ${passwordFile}
+        chown root:root ${passwordFile}
+        chmod 0400 ${passwordFile}
+      fi
+
+      if [ ! -s ${htpasswdFile} ]; then
+        htpasswd -B -i -c ${htpasswdFile} john < ${passwordFile}
+        chown root:nginx ${htpasswdFile}
+        chmod 0440 ${htpasswdFile}
+      fi
+    '';
+  };
+
   services.searx = {
     enable = true;
     redisCreateLocally = true;
@@ -64,6 +99,17 @@ in
         ];
       };
 
+      engines = [
+        {
+          name = "duckduckgo";
+          disabled = true;
+        }
+        {
+          name = "google";
+          disabled = true;
+        }
+      ];
+
       ui.static_use_hash = true;
     };
   };
@@ -71,8 +117,11 @@ in
   services.nginx.virtualHosts."${domain}" = {
     forceSSL = true;
     enableACME = true;
+    basicAuthFile = htpasswdFile;
     locations."/" = {
-      return = "403";
+      proxyPass = "http://127.0.0.1:8888";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
     };
     extraConfig = ''
       access_log off;
